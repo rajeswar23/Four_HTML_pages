04fd5156a3c08f2f5f00cc155ff82b042b67c07a
rajeswar23 <chembetisudheer23@gmail.com>
2023-11-10 16:42:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def four1(request):
+    return render(request,'four1.html')
+
+def four2(request):
+    return render(request,'four2.html')
+
+def four3(request):
+    return render(request,'four3.html')
+
+def four4(request):
+    return render(request,'four4.html')
